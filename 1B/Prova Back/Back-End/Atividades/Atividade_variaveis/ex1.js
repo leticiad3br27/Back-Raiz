96de3623d062b38beb1bc844676c9cda3e2227ba
Leticia2711 <leticia27belo11@gmail.com>
2024-10-31 10:12:03 -0400

    editei

--- 1B/Prova Back/Back-End/Atividades/Atividade_variaveis/ex1.js
+++
@@ -1,7 +0,0 @@
-/*Escreva um programa que declare duas variáveis, “nome” e “idade”, e as 
-imprima em um console em uma frase que diga “Olá, meu nome é [nome] 
-e eu tenho [idade] anos”.*/
-
-let nome = "Hiago Gabriel";
-let idade = 16;
-console.log("Olá, meu nome é",nome,"e eu tenho ",idade, "anos.")
--- 1B/Prova Back/Back-End/Atividades/Atividade_variaveis/ex2.js
+++
@@ -1,5 +0,0 @@
-/*Declare uma string e utilize métodos para converter para maiúsculas e 
-minúsculas.*/
-
-let msg = "O rato reu a roupa do rei de Rôma";
-console.log(`Normal: ${msg}\nMaiúscula: ${msg.toUpperCase()}\nMinúscula: ${msg.toLowerCase()}`);
